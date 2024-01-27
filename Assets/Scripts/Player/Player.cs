79924b3e03bb1537a440a56069665e8fd4aa478b
Ryousei <hosinorubi@gmail.com>
2024-01-27 16:02:54 +0900

    [add] Player Animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Playables;
 
 public class Player : MonoBehaviour
 {
@@ -9,20 +10,58 @@ public class Player : MonoBehaviour
     private float walkSpeed = 1;
     [SerializeField, Header("ジャンプ速度")]
     private float jumpSpeed = 1;
+    // ダッシュ時のサウンドを指定します。
+    [SerializeField]
+    private AudioClip soundOnJump = null;
 
 
-    //　先に宣言しておく変数
+    // プレイヤーの状態を表します。
+    enum PlayerState
+    {
+        // 立ち状態
+        Stand,
+        // 通常の走行状態
+        Walk,
+        // ダッシュ時の走行状態
+        Jump,
+        // ジャンプの予備動作
+        JumpReady,
+        // 地面から足が離れて上昇中
+        Jumping,
+    }
+    // 現在のプレイヤー状態
+    PlayerState currentState = PlayerState.Stand;
+
+    //　コンポーネントを参照しておく変数
     Animator animator;
     Vector2 inputDirection;
     Rigidbody2D rb;
     RunGame2023 inputSys;
     bool isJump;
+    bool isWalk;
+    AudioSource audioSource;
+
 
     // AnimatorのパラメーターID
     static readonly int isWalkId = Animator.StringToHash("isWalk");
     static readonly int isRunId = Animator.StringToHash("isRun");
-    static readonly int jumpId = Animator.StringToHash("Jump");
+    static readonly int jumpId = Animator.StringToHash("isJump");
     static readonly int landingId = Animator.StringToHash("Landing");
+
+    // Jumpステートに遷移させます。
+    void SetJumpState()
+    {
+        currentState = PlayerState.JumpReady;
+        animator.SetBool(jumpId, true);
+
+        // ジャンプ音を再生
+        //if (audioSource.isPlaying)
+        //{
+        //    audioSource.Stop();
+        //}
+        //audioSource.PlayOneShot(soundOnJump);
+        //audioSource.Play();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -37,19 +76,67 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        // 状態ごとの分岐処理
+        switch (currentState)
+        {
+            case PlayerState.Stand:
+                UpdateForStandState();
+                break;
+            case PlayerState.Walk:
+                UpdateForWalkState();
+                break;
+            case PlayerState.JumpReady:
+                UpdateForJumpReadyState();
+                break;
+            case PlayerState.Jumping:
+                UpdateForJumpingState();
+                break;
+            default:
+                break;
+        }
+
     }
 
-    private void Move()
+    private void UpdateForStandState()
+    {
+        currentState = PlayerState.Walk;
+    }
+
+    // Walk状態のフレーム更新処理です。
+    private void UpdateForWalkState()
     {
         rb.velocity = new Vector2(inputDirection.x * walkSpeed, rb.velocity.y);
+        
+        animator.SetBool(isWalkId, inputDirection.x != 0.0f);
+
+        if(!isJump)
+        {
+            SetJumpState();
+        }
+    }
+
+    // JumpReady状態のフレーム更新処理です。
+    void UpdateForJumpReadyState()
+    {
+        Debug.Log("ジャンプ");
+        currentState = PlayerState.Jumping;
+    }
+
+    // Jumping状態のフレーム更新処理です。
+    void UpdateForJumpingState()
+    {
+        Debug.Log("ジャンプからの着地");
+        animator.SetTrigger(landingId);
+        currentState = PlayerState.Stand;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
+            Debug.Log("地面");
             isJump = false;
+            //animator.SetBool(jumpId, isJump);
         }
     }
 
@@ -59,7 +146,7 @@ public class Player : MonoBehaviour
         animator.SetBool(isWalkId, true);
     }
 
-    public void OnJumo(InputAction.CallbackContext context)
+    public void OnJump(InputAction.CallbackContext context)
     {
         if (!context.performed || isJump)
         {
@@ -68,6 +155,6 @@ public class Player : MonoBehaviour
 
         rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
         isJump = true;
-        animator.SetBool(isRunId, true);
+        animator.SetBool(jumpId, isJump);
     }
 }
